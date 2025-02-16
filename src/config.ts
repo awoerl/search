476f8545fd6f0952a2b65d150e499b498938d417
Alexander Wörl <alexander.woerl@contentserv.com>
2025-02-16 09:49:51 +0100

    Introducing ProjectID

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,14 @@
-export const BASE_URL = "https://pba371.saas.contentserv.com/admin/rest";
+
+const getProjectID = () => {
+  let newProjectID = window.location.hash.substring(1);
+  if (newProjectID) {
+    localStorage.setItem('projectID', newProjectID);
+  }
+  let projectID = localStorage.getItem('projectID') || 'pba371';
+  return projectID;
+}
+
+export var BASE_URL = "https://" + getProjectID() + ".saas.contentserv.com/admin/rest";
 export const CS_USER_NAME = 'admin';
 export const CS_PASSWORD = 'admin';
 export var IMAGE_MODE = 'cover'; //cover, contain
