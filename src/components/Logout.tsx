7525be05a4a16145a5d69cc3f7bb27c18ab59f85
Alexander Wörl <alexander.woerl@contentserv.com>
2025-02-18 16:21:36 +0100

    Changes

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -2,14 +2,7 @@ import { ImSwitch } from "react-icons/im";
 import { BASE_URL } from "../core/config";
 
 const logout = () => {
-  let targetUrl = "";
-  if (BASE_URL.includes("contentserv.com")) {
-    targetUrl = "https://login.contentserv.com/?instancelogout=true";
-  } else {
-    targetUrl = BASE_URL + "/..?login=now";
-  }
-
-  window.open(targetUrl, "_top");
+  window.open(BASE_URL + "/..?login=now", "_top");
 };
 
 const Logout = () => {
